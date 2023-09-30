0aecf882d0a5e4079d221631a75e9b038d9fbed3
gtildis <gretil_15@hotmail.com>
2023-09-30 18:51:49 +0200

    initial setup

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/globals.css";
+
+export const metadata = {
+	title: "Promptopia",
+	description: "Discover & Share AI Prompts",
+};
+
+const layout = () => {
+	return (
+		<html lang="en">
+			<body>
+				<div className="main">
+					<div className="gradient" />
+				</div>
+
+				<main></main>
+			</body>
+		</html>
+	);
+};
+
+export default layout;
